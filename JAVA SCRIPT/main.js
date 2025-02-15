bc9c69de0688b3c96020be19946cba03f187fe24
Abdulraheem Salah <abdulraheem.salaah@gmail.com>
2025-02-15 18:56:43 +0400

    ashraf

--- JAVA SCRIPT/main.js
+++ JAVA SCRIPT/main.js
@@ -60,21 +60,26 @@ ScrollReveal().reveal(".section__container h1",{
             });
 
             
-            document.addEventListener("DOMContentLoade", function (event) {
-                let circle = document.querySelectorAll(".circle");
-                circle.forEach(function (progress) {
-                  let degree = 0;
-                  var targetDegree = parseInt(progress.getAttribute("data-degree"));
-                  let color = progress.getAttribute("data-color");
-                  let number = progress.querySelector(".number");
-        
-                  var interval = setInterval(function () {
+          document.addEventListener("DOMContentLoaded", function
+            (event){
+              let circle = document.querySelectorAll('.circle');
+              circle.forEach(function(progress){
+                let degree = 0;
+                var targetDegree = parseInt(progress.getAttribute
+                  ('data-degree'));
+                  let color = progress.getAttribute('data-color');
+                  let number = progress.querySelector('.number');
+
+                  var interval = setInterval(function(){
                     degree += 1;
-                    if (degree > targetDegree) {
+                    if (degree > targetDegree){
                       clearInterval(interval);
                       return;
                     }
                     progress.style.background = `conic-gradient(${color} ${degree}%, #222 0%)`;
-                  }, 50);
-                });
-              });
+                    number.innerHTML = degree + '<span>%</span>';
+                    number.style.color = white;
+                  },50)
+              })
+
+          })
