87cb2b77f2e8b9d71047f002ac0f9c709aea4ce5
Abdulraheem Salah <abdulraheem.salaah@gmail.com>
2025-04-11 20:00:31 +0400

    updates 11-4-2025

--- js/main.js
+++ js/main.js
@@ -10,6 +10,16 @@ var finalPath = `M 10 100 Q 500 1000 490 100`;
 var path = `M 10 100 Q 500 100 990 100`;
 var finpath = `M 10 100 Q 500 100 990 100`;
 
+
+
+
+
+
+
+
+
+
+
 var string = document.querySelector(".cv");
 string.addEventListener("mousemove", function (dets) {
     path = `M 10 100 Q 500 ${dets.y} 990 100`;
