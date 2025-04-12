7fc96d5b9e2ca3795a3d8487d687ba641c3a829c
Abdulraheem Salah <abdulraheem.salaah@gmail.com>
2025-04-12 21:01:52 +0400

    new collections with stars animations

--- js/main.js
+++ js/main.js
@@ -17,34 +17,8 @@ var finpath = `M 10 100 Q 500 100 990 100`;
 
 
 
-
-
-
-var string = document.querySelector(".cv");
-string.addEventListener("mousemove", function (dets) {
-    path = `M 10 100 Q 500 ${dets.y} 990 100`;
-    gsap.to(".cv svg path", {
-        attr: { d: path },
-        duration: 0.2,
-        ease: "power3.out"
-    })
-
-
-})
-string.addEventListener("mouseleave", function () {
-
-    gsap.to(".cv svg path", {
-        attr: { d: finpath },
-        duration: 0.3,
-        ease: "elastic.out(1,0.2"
-
-    })
-
-})
-
+//  CURSOR MOVING CIRCLE
 main.addEventListener("mousemove", function (moves) {
-
-
     gsap.to(cursor, {
         x: moves.x,
         y: moves.y,
@@ -55,6 +29,9 @@ main.addEventListener("mousemove", function (moves) {
 })
 
 
+
+
+// FULL NAVIAGATION MENU LINKS ANIMATION
 const ani = function () {
     tl.from(".nav-item1", {
         y: -10,
@@ -82,17 +59,104 @@ const ani = function () {
     })
 }
 document.onload = ani();
-gsap.from("#cv ", {
-    y: 60,
+
+
+//   stars animation
+
+    gsap.to(".star" ,{
+        x:1400,
+        y:1000,
+        scale:0.1,
+        opacity: 0,
+        duration: 10,
+        delay:0.2
+    })
+    gsap.to(".star2" ,{
+        x:1800,
+        y:300,
+        scale:0.1,
+        opacity: 0,
+        delay:0.5,
+        duration: 8})
+    gsap.to(".star3" ,{
+        x:1600,
+        y:200,
+        scale:0.1,
+        opacity: 0,
+        delay:1,
+        duration: 8})
+    gsap.to(".star4" ,{
+        x:1600,
+        y:300,
+        scale:0.1,
+        opacity: 0,
+        delay:0.1,
+        duration: 8})
+    gsap.to(".star5" ,{
+        x:1800,
+        y:400,
+        scale:0.1,
+        opacity: 0,
+        delay:1.5,
+        duration: 8})
+    gsap.to(".star6" ,{
+        x:1900,
+        y:200,
+        scale:0.1,
+        opacity: 0,
+        delay:1.5,
+        duration: 8})
+    
+    gsap.to(".star7" ,{
+        x:2000,
+        y:200,
+        scale:0.1,
+        opacity: 0,
+        delay:1.5,
+        duration: 8})
+
+
+
+
+
+
+
+// var string = document.querySelector(".cv");
+// string.addEventListener("mousemove", function (dets) {
+//     path = `M 10 100 Q 500 ${dets.y} 990 100`;
+//     gsap.to(".cv svg path", {
+//         attr: { d: path },
+//         duration: 0.2,
+//         ease: "power3.out"
+//     })
+
+
+// })
+// string.addEventListener("mouseleave", function () {
+
+//     gsap.to(".cv svg path", {
+//         attr: { d: finpath },
+//         duration: 0.3,
+//         ease: "elastic.out(1,0.2"
+
+//     })
+
+// })
+
+
+
+// ======================================================
+
+gsap.from("#cv-title ", {
+    x: -800,
     opacity: 0,
+    scale:0,
     scrollTrigger: {
-        trigger: "#cv",
+        trigger: "#cv-title",
         scroller: "body",
-
         start: "top 85%",
-        end: "top 55%",
-        scrub: 1
-
+        end: "top 20%",
+        scrub:2
     }
 })
 
